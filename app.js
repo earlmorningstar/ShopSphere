e11e5be16dc19572371f56fdd407bbc844268b91
Onyeabor Joel <earlmorningstar@gmail.com>
2023-08-28 02:16:56 +0100

    Mere toggle button.

---
+++ app.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menu-toggle");
+const dashboard = document.getElementById("dashboard");
+
+function toggleDashboard() {
+  dashboard.classList.toggle("active");
+}
+
+menuToggle.addEventListener("click", toggleDashboard);
